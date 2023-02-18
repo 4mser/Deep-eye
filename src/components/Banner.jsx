b9df29abecac3988dd24f5a88562c3c97238b743
4mser <nicolas.moreno.sagan@gmail.com>
2023-02-18 10:49:57 -0300

    sensibilidad ajustada para moviles

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+/* import React, { useState, useEffect } from "react";
 
 
 const Banner = ({theme}) => {
@@ -63,4 +63,86 @@ const Banner = ({theme}) => {
     )
 }
 
-export default Banner
+export default Banner */
+
+import React, { useState, useEffect } from "react";
+
+const Banner = ({theme}) => {
+
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [orientation, setOrientation] = useState(null);
+
+  useEffect(() => {
+    const handleMouseMove = (event) => {
+      setPosition({
+        x: event.clientX,
+        y: event.clientY,
+      });
+    };
+    const handleOrientation = (event) => {
+      setPosition({
+        x: (event.gamma + 90) * 5,
+        y: (event.beta + 90) * 5,
+      });
+    };
+    if (window.innerWidth > 1000) {
+      window.addEventListener("mousemove", handleMouseMove);
+      return () => {
+        window.removeEventListener("mousemove", handleMouseMove);
+      };
+    } else {
+      window.addEventListener("deviceorientation", handleOrientation);
+      return () => {
+        window.removeEventListener("deviceorientation", handleOrientation);
+      };
+    }
+  }, []);
+
+  useEffect(() => {
+    const handleOrientationChange = () => {
+      setOrientation(window.screen.orientation.type);
+    };
+    window.addEventListener("orientationchange", handleOrientationChange);
+    return () => {
+      window.removeEventListener("orientationchange", handleOrientationChange);
+    };
+  }, []);
+
+  const sensitivity =
+    window.innerWidth > 1000
+      ? { x: 200, y: 200 }
+      : orientation === "landscape-primary" || orientation === "landscape-secondary"
+      ? { x: 100, y: 50 }
+      : { x: 50, y: 100 };
+  
+  let x = 0;
+  let y = 0;
+  let limitedX = 0;
+  let limitedY = 0;
+  x = (position.x - window.innerWidth / 2) / sensitivity.x;
+  y = (position.y - window.innerHeight / 2) / sensitivity.y;
+  limitedX = Math.min(Math.max(x, -25), 25);
+  limitedY = Math.min(Math.max(y, -25), 25);
+
+  return (
+    <div>
+      <section className="banner-container">
+        <div
+          className={theme === "dark" ? "banner-dark" : "banner-light"}
+          style={{
+            transform: `translate(${-limitedX}%, ${-limitedY}%)`,
+            transition: "transform .1s ease-out",
+          }}
+        >
+          <img
+            src="https://s3.amazonaws.com/skybar.database.teset.1/imgBanner-02.png"
+            alt="Deep-Eye-logo"
+            border="0"
+          />
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Banner;
