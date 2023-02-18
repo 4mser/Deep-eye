b19fcff2bcbf7ce3a6858f614b03b18c8950910b
4mser <nicolas.moreno.sagan@gmail.com>
2023-02-18 13:13:28 -0300

    test9

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -69,7 +69,7 @@ import React, { useState, useEffect } from "react";
 
 const Banner = ({theme}) => {
 
-  const [position, setPosition] = useState({ x: 0, y: 0 });
+/*   const [position, setPosition] = useState({ x: 0, y: 0 });
   const [orientation, setOrientation] = useState(null);
 
   useEffect(() => {
@@ -81,8 +81,8 @@ const Banner = ({theme}) => {
     };
     const handleOrientation = (event) => {
       setPosition({
-        x: (event.gamma + 90) * 10,
-        y: (event.beta + 90) * 10,
+        x: (event.gamma + 90) * 5,
+        y: (event.beta + 90) * 5,
       });
     };
     if (window.innerWidth > 1000) {
@@ -122,7 +122,48 @@ const Banner = ({theme}) => {
   x = (position.x - window.innerWidth / 2) / sensitivity.x;
   y = (position.y - window.innerHeight / 2) / sensitivity.y;
   limitedX = Math.min(Math.max(x, -25), 25);
-  limitedY = Math.min(Math.max(y, -25), 25);
+  limitedY = Math.min(Math.max(y, -25), 25); */
+
+  const sensitivity = window.innerWidth > 1000 ? { x: 10, y: 20 } : { x: 10, y: 10 };
+
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  const handleMouseMove = (event) => {
+    setPosition({
+      x: event.clientX,
+      y: event.clientY,
+    });
+  };
+
+  const handleOrientation = (event) => {
+    setPosition({
+      x: (event.gamma + 90) * 5,
+      y: (event.beta + 90) * 5,
+    });
+  };
+
+  useEffect(() => {
+    if (window.innerWidth > 1000) {
+        window.addEventListener("mousemove", handleMouseMove);
+        return () => {
+          window.removeEventListener("mousemove", handleMouseMove);
+        };
+      } else {
+        window.addEventListener("deviceorientation", handleOrientation);
+        return () => {
+          window.removeEventListener("deviceorientation", handleOrientation); 
+        };
+      }
+    }, []);
+
+    let x = 0;
+    let y = 0;
+    let limitedX = 0;
+    let limitedY = 0;
+    x = (position.x - window.innerWidth / 2) / sensitivity.x;
+    y = (position.y - window.innerHeight / 2) / sensitivity.y;
+    limitedX = Math.min(Math.max(x, -25), 25);
+    limitedY = Math.min(Math.max(y, -25), 25);
 
   return (
     <div>
