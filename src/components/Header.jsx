4028ea9f7bd9df0567ff6a03e13e80c844b5bdca
4mser <nicolas.moreno.sagan@gmail.com>
2023-02-21 13:21:19 -0300

    menu ahora se cierra al apretar una ruta

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,6 +19,8 @@ const Header = ({ onThemeChange, theme }) => {
 
     const location = useLocation();
 
+    
+
     let iconoInicio = location.pathname === '/deep-eye/' ? <AiFillHome className='icon' /> : <AiOutlineHome className='icon' />;
   let iconoSobre = location.pathname === '/deep-eye/sobre/' ? <MdAnnouncement className='icon' /> : <MdOutlineAnnouncement className='icon' />;
   let iconoTienda = location.pathname === '/deep-eye/tienda/' ? <RiShoppingCart2Fill className='icon' /> : <RiShoppingCart2Line className='icon' />;
@@ -113,7 +115,7 @@ const sensitivity = window.innerWidth > 1000 ? { x:83} : { x: 10000};
                     <ul className={`nav ${theme === 'dark' ? 'dark' : 'light'}`}>
 
                         <li>
-                            <Link to='/deep-eye/'> 
+                            <Link to='/deep-eye/' onClick={handleClick}> 
                                 <div className="icon-p">
                                     {iconoInicio}
                                     <p>Inicio</p>
@@ -122,7 +124,7 @@ const sensitivity = window.innerWidth > 1000 ? { x:83} : { x: 10000};
                         </li>
 
                         <li>
-                            <Link to='/deep-eye/sobre/'>
+                            <Link to='/deep-eye/sobre/' onClick={handleClick}>
                                 <div className="icon-p">
                                     {iconoSobre} 
                                     <p>Sobre</p>
@@ -150,56 +152,56 @@ const sensitivity = window.innerWidth > 1000 ? { x:83} : { x: 10000};
                                 style={{ transform: `translate(${-limitedX}%`,
                                 transition: "transform 0.1s ease-out" }}>
                                 
-                                <Link to='/deep-eye/tips&trips/'>
+                                <Link to='/deep-eye/tips&trips/' onClick={handleClick}>
                                     <li>
                                         <p>Tips & Trips</p> 
                                         <img src="https://s3.amazonaws.com/skybar.database.teset.1/psychedelic-eye.gif" alt="" />
                                     </li>
                                 </Link>
 
-                                <Link to='/deep-eye/psi&neuro/'>
+                                <Link to='/deep-eye/psi&neuro/' onClick={handleClick}>
                                     <li>
                                         <p>Psicología & Neurociencia</p>  
                                         <img src="https://s3.amazonaws.com/skybar.database.teset.1/MRI-brain-wp.gif" alt="" />
                                     </li>
                                 </Link>
 
-                                <Link to='/deep-eye/psicodelicos/'>
+                                <Link to='/deep-eye/psicodelicos/' onClick={handleClick}>
                                     <li id='psicodelicos'>
                                         <p>Psicodélicos</p> 
                                         <img src="https://s3.amazonaws.com/skybar.database.teset.1/dc7uw9n-135a4c5d-11da-47c4-8eae-d29111d07fbe.gif" alt="" />
                                     </li>
                                 </Link>
 
-                                <Link to='/deep-eye/meditacion/'>
+                                <Link to='/deep-eye/meditacion/' onClick={handleClick}>
                                     <li>
                                         <p>Meditación & Respiración</p> 
                                         <img id='meditation-gif' src="https://s3.amazonaws.com/skybar.database.teset.1/409F.gif" alt="" />
                                     </li>
                                 </Link>
 
-                                <Link to='/deep-eye/respiracion'>
+                                <Link to='/deep-eye/respiracion' onClick={handleClick}>
                                     <li>
                                         <p>Inteligencia Artificial</p>  
                                         <img src="https://s3.amazonaws.com/skybar.database.teset.1/GIF3.gif" alt="" />
                                     </li>
                                 </Link>
 
-                                <Link to='/deep-eye/sueños/'>
+                                <Link to='/deep-eye/sueños/' onClick={handleClick}>
                                     <li>
                                         <p>Sueños</p>  
                                         <img src="https://s3.amazonaws.com/skybar.database.teset.1/dr-strange-trippy.gif" alt="" />
                                     </li>
                                 </Link>
 
-                                <Link to='/deep-eye/arte/'>
+                                <Link to='/deep-eye/arte/' onClick={handleClick}>
                                     <li>
                                         <p>Arte & Diseño</p>  
                                         <img src="https://s3.amazonaws.com/skybar.database.teset.1/8cbd7e449e8cbcd45368837ebab7ba5b.gif" alt="" />
                                     </li>
                                 </Link>
 
-                                <Link to='/deep-eye/arte/'>
+                                <Link to='/deep-eye/arte/' onClick={handleClick}>
                                     <li>
                                         <p>Contenido recomendado</p>  
                                         <img src="https://s3.amazonaws.com/skybar.database.teset.1/pretty-book-bench-nature-water-outdoors-animated-gif.gif" alt="" />
@@ -213,7 +215,7 @@ const sensitivity = window.innerWidth > 1000 ? { x:83} : { x: 10000};
                         </li>
 
                         <li>
-                            <Link to='/deep-eye/tienda/'> 
+                            <Link to='/deep-eye/tienda/' onClick={handleClick}> 
                                 <div className="icon-p">
                                     {iconoTienda}
                                     <p>Tienda</p>
